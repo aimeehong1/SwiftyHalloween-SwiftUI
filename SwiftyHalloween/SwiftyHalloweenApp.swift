5e1ce53349009025490c7610a1a58586d602a48f
Aimee Hong <honglp@bc.edu>
2024-10-07 15:00:21 -0400

    Initial Commit

---
+++ SwiftyHalloween/SwiftyHalloweenApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyHalloweenApp.swift
+//  SwiftyHalloween
+//
+//  Created by Aimee Hong on 10/7/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyHalloweenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftyHalloweenTests/SwiftyHalloweenTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyHalloweenTests.swift
+//  SwiftyHalloweenTests
+//
+//  Created by Aimee Hong on 10/7/24.
+//
+
+import Testing
+@testable import SwiftyHalloween
+
+struct SwiftyHalloweenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
